425488fb18af42fdf32f6720fe83446024f17655
yurlov43 <yurlov43@gmail.com>
2021-12-01 07:47:41 +0300

    added instance methods

--- validation.rb
+++ validation.rb
@@ -3,19 +3,18 @@ module Validation
     base.include InstanceMethods
     base.extend ClassMethods
     base.class_variable_set(:@@attributes, {})
-    base.instance_variable_set(:@errors, [])
   end
 
   module InstanceMethods
     def validate!
-      self.class.errors = nil
+      self.errors = nil
       self.class.attributes.each do |attribute_name, methods|
-        methods.each { |validation_type, parameter| self.class.send(validation_type, attribute_name, parameter) }
+        methods.each { |validation_type, parameter| self.send(validation_type, attribute_name, parameter) }
       end
-      raise self.class.errors.join("\n") unless self.class.errors.empty?
-    rescue RuntimeError => error
-      puts error.message
-      raise error
+      unless errors.empty?
+        puts errors
+        raise errors.join("\n") unless errors.empty?
+      end
     end
 
     def valid?
@@ -24,42 +23,44 @@ module Validation
     rescue RuntimeError
       false
     end
-  end
 
-  module ClassMethods
-    def attributes
-      self.class_variable_get(:@@attributes)
-    end
+    protected
 
     def errors
       @errors
     end
 
     def errors=(value)
-      value.nil? ? @errors = [] : @errors << value
+      @errors.nil? || value.nil? ? @errors = [] : @errors << value
     end
 
     define_method(:presence) do |attribute_name, parameter|
       if attribute_name.nil? || attribute_name == ''
-        self.errors = "Attribute name not specified."
+        self.errors = "#{attribute_name}: attribute name not specified."
       end
     end
 
     define_method(:format) do |attribute_name, parameter|
       unless !parameter.nil? && attribute_name.to_s =~ parameter
-        self.errors = "Invalid attribute value."
+        self.errors = "#{attribute_name}: invalid attribute value."
       end
     end
 
     define_method(:type) do |attribute_name, parameter|
       unless !parameter.nil? && attribute_name.instance_of?(parameter)
-        self.errors = "The attribute value does not match the specified class."
+        self.errors = "#{attribute_name}: the attribute value does not match the specified class."
       end
     end
+  end
 
+  module ClassMethods
     def validate(attribute_name, validation_type, *parameter)
       self.attributes[attribute_name] ||= {}
       self.attributes[attribute_name][validation_type] = parameter.first
+    end
+
+    def attributes
+      self.class_variable_get(:@@attributes)
     end
   end
 end
